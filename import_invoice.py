e1f147d5c8b62d2390157d412e625aa1fecf3d17
elalaoui <aelalaoui@agilorg.com>
2015-04-24 18:37:13 +0100

    commit 3

--- import_invoice.py
+++ import_invoice.py
@@ -14,29 +14,35 @@ class import_invoice(osv.osv):
             }
     
     def create(self, cr, uid, vals, context = None): 
-        print "entree :",vals
         v=vals['account_id']
+        complementary_invoice_ids=vals['complementary_invoice_ids'][0][2]
+        brws=self.pool.get('complementary.invoice').browse(cr, uid, complementary_invoice_ids)
         fact_imp_line_obj= self.pool.get('import.invoice.lines')
-        print fact_imp_line_obj
         inv = self.pool.get('account.invoice').browse(cr, uid, v)
-        print inv
         factre_improt_line_ids=[]
+        val={}
+        mt_frais =0
         for line in inv.invoice_line:
-            val={}
-            print 'product id :',line.product_id.id
-            print 'quantity of product : ',line.quantity
-            print 'product price :',line.product_id.product_tmpl_id.list_price
-            val['itms_id']=line.product_id.id
-            print "vals['itms_id']==",val['itms_id']
-            val['montant_achat']= line.price_subtotal
-            print "vals['montant_achat']==",val['montant_achat']
-            id=fact_imp_line_obj.create(cr, uid,val)
-            print "ID==",id
-            factre_improt_line_ids.append(id)
+                for rec in brws:
+                    print rec
+                    ecl = rec.bursting_type
+                    if ecl == "cout":
+                        mt_frais += rec.montant_id / line.price_subtotal
+                    else :
+                        mt_frais += rec.montant_id / line.quantity
+                val['itms_id']=line.product_id.id
+                print val['itms_id']
+                val['montant_achat']= line.price_subtotal
+                print val['montant_achat']
+                val['montant_frais']= val['montant_achat']+mt_frais
+                print val['montant_frais']
+                id=fact_imp_line_obj.create(cr, uid,val)
+                print "ID==",id
+                factre_improt_line_ids.append(id)    
         vals['import_invoice_lines_ids']=[[6, False, factre_improt_line_ids]]
+        print "sortie ",vals
         #print vals['import_invoice_lines_ids']
         #print factre_improt_line_ids
-        print "sortie :",vals
         return super(import_invoice, self).create(cr, uid, vals, context)
     
     
